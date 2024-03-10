f3079c82ae8486d5daee738ddd85c5441fbc241e
Arrowin24 <igor_rudenko.ru@mail.ru>
2024-03-10 20:24:22 +0700

    finish first app

--- app/src/main/java/com/arrowin/beeradviser/MainActivity.kt
+++ app/src/main/java/com/arrowin/beeradviser/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.arrowin.beeradviser
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import android.widget.TextView
+
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val findBeer = findViewById<Button>(R.id.find_beer)
+        findBeer.setOnClickListener {
+            val beerColor = findViewById<Spinner>(R.id.beer_color)
+            val color = beerColor.selectedItem.toString()
+            val brands = findViewById<TextView>(R.id.brands)
+            val beerList = getBeers(color)
+            val beer = beerList.reduce {first, item -> first + "hello" +'\n'+item}
+            brands.text = "$beer"
+        }
+    }
+    fun getBeers(color:String): List<String>{
+        return when (color){
+            "Light" -> listOf("Jail Pale Ale", "Lager Lite")
+            "Amber" -> listOf("Jack Amber", "Red Moose")
+            "Brown" -> listOf("Brown Bear Beer", "Bock Brownie")
+            else -> listOf("Gout Stout", "Dark Daniel")
+        }
     }
 }
